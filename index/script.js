ce17eede29fd63723a71a68c0876fdcee2294453
raj-ad1tya <108735519+raj-ad1tya@users.noreply.github.com>
2022-07-17 01:24:57 +0530

    Update script.js

--- index/script.js
+++ index/script.js
@@ -1,11 +1,15 @@
 var repoLinks = [
     'https://github.com/raj-ad1tya/nagarro/tree/main/portfolio',
-    'https://github.com/raj-ad1tya/nagarro/tree/main/pokemon'
+    'https://github.com/raj-ad1tya/nagarro/tree/main/pokemon',
+    'https://github.com/raj-ad1tya/nagarro/tree/main/to-do-js',
+    'https://github.com/raj-ad1tya/nagarro/tree/main/to-do-jquery'
 ]
 
 var hostedLinks = [
     'https://raj-ad1tya.github.io/nagarro/portfolio/',
-    'https://raj-ad1tya.github.io/nagarro/pokemon/'
+    'https://raj-ad1tya.github.io/nagarro/pokemon/',
+    'https://raj-ad1tya.github.io/nagarro/to-do-js/',
+    'https://raj-ad1tya.github.io/nagarro/to-do-jquery/'
 ]
 
 $(document).ready(function() {
